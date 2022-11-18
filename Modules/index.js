c74902630ead6c09626f0c0395e2e33f5cde6233
Adarsh <jha01adarsh@gmail.com>
2022-11-18 22:38:48 +0530

    Print hello world + modules

---
+++ Modules/index.js
@@ -0,0 +1,4 @@
+const operation = require("./operation");
+
+console.log(operation.addition(2,3));
+console.log(operation.multiplication(3,3));
---
+++ Modules/operation.js
@@ -0,0 +1,7 @@
+module.exports.addition = function add(a,b){
+    return a+b;
+}
+
+module.exports.multiplication = function(a,b){
+    return a*b;
+}
